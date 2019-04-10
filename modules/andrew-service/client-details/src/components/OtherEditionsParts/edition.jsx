b7887feba7909ebd6d04454fb2b54c352e0024cc
ahchung23 <hahneol@gmail.com>
2019-04-10 05:22:37 -0400

    clean up code, delete commented out styled-comp code

--- modules/andrew-service/client-details/src/components/OtherEditionsParts/edition.jsx
+++ modules/andrew-service/client-details/src/components/OtherEditionsParts/edition.jsx
@@ -1,41 +1,45 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import EditionTooltip from './editionTooltip.jsx';
+import styles from '../css/Edition.less';
 
-const ImgLiWrapper = styled.li`
-display: inline-block;
-list-style-type: none;
-margin: 0px;
-padding-top: 3px;
-padding-right: 2px;
-position: relative;
-`;
+// const ImgLiWrapper = styled.li`
+// display: inline-block;
+// list-style-type: none;
+// margin: 0px;
+// padding-top: 3px;
+// padding-right: 2px;
+// position: relative;
+// `;
 
-const ImgWrapper = styled.img`
-  max-width: 55px;
-  height: 55px;
-  border: 0;
-`;
+// const ImgWrapper = styled.img`
+//   max-width: 55px;
+//   height: 55px;
+//   border: 0;
+// `;
 
-const TooltipWrapper = styled.div`
-  display: none;
+// const TooltipWrapper = styled.div`
+//   display: none;
 
-  ${ImgLiWrapper}:hover & {
-    display: block;
-  }
-`;
+//   ${ImgLiWrapper}:hover & {
+//     display: block;
+//   }
+// `;
 
 const Edition = (props) => {
   const { editionData } = props;
   const { id, coverurl, title } = editionData;
 
   return (
-    <ImgLiWrapper key={id}>
-      <ImgWrapper className="editionImg" key={id} src={coverurl} alt={title} />
-      <TooltipWrapper>
-        <EditionTooltip {...editionData} />
-      </TooltipWrapper>
-    </ImgLiWrapper>
+    <li className={styles.editionImgLiWrapper} key={id}>
+      <div className={styles.editionImgWrapper}>
+        <img className={styles.editionImg} key={id} src={coverurl} alt={title} />
+
+        <div className={styles.tooltipWrapper}>
+          <EditionTooltip {...editionData} />
+        </div>
+      </div>
+    </li>
   );
 };
 
