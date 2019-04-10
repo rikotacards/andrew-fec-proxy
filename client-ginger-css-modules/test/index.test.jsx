be98395723275e56b6b1471100ab4bd6cfdbc78f
ahchung23 <hahneol@gmail.com>
2019-04-10 17:25:00 -0400

    delete client folder from proxy-service

--- client-ginger-css-modules/test/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../components/App.jsx';
-import { shallow, mount, render } from 'enzyme';
-
-
-test('<App />', () => {
-  const wrapper = shallow(<App /> )
-  expect(wrapper.exists()).toBe(true);
-})
