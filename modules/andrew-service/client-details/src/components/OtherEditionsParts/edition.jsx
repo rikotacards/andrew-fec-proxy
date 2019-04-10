613f2335540cea3a50068bf0708bb755262c5185
ahchung23 <hahneol@gmail.com>
2019-04-10 05:23:56 -0400

    clean up styled-components code from OtherEditionParts folder

--- modules/andrew-service/client-details/src/components/OtherEditionsParts/edition.jsx
+++ modules/andrew-service/client-details/src/components/OtherEditionsParts/edition.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
-// import styled from 'styled-components';
 import EditionTooltip from './editionTooltip.jsx';
 import styles from '../css/Edition.less';
-
-// const ImgLiWrapper = styled.li`
-// display: inline-block;
-// list-style-type: none;
-// margin: 0px;
-// padding-top: 3px;
-// padding-right: 2px;
-// position: relative;
-// `;
-
-// const ImgWrapper = styled.img`
-//   max-width: 55px;
-//   height: 55px;
-//   border: 0;
-// `;
-
-// const TooltipWrapper = styled.div`
-//   display: none;
-
-//   ${ImgLiWrapper}:hover & {
-//     display: block;
-//   }
-// `;
 
 const Edition = (props) => {
   const { editionData } = props;
