655d81807e19e0116365894063e0c5bbfc26dce9
ahchung23 <hahneol@gmail.com>
2019-04-11 17:09:11 -0400

    setup grunt to auto deploy to amazonS3!

--- modules/andrew-service-details/Gruntfile.js
+++ modules/andrew-service-details/Gruntfile.js
@@ -23,18 +23,44 @@ module.exports = function (grunt) {
 
     webpack: {
       options: {
-        stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+        stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
       },
       prod: webpackProdConfig,
       dev: Object.assign({ watch: true }, webpackDevConfig),
+    },
+
+    aws: grunt.file.readJSON('awss3.json'),
+
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.AWSAccessKeyId %>',
+        secretAccessKey: '<%= aws.AWSSecretKey %>',
+      },
+      dist: {
+        options: {
+          bucket: 'fec-details-module',
+          signatureVersion: 'v4',
+          region: 'us-east-2',
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'public/',
+            src: ['**'],
+            dest: '/',
+          },
+        ],
+      },
     },
   });
 
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-run');
+  grunt.loadNpmTasks('grunt-aws-s3');
 
   grunt.registerTask('prod', ['run:seed', 'webpack:prod', 'run:server']);
-
   grunt.registerTask('dev', ['run:seed', 'webpack:dev']);
   grunt.registerTask('server', ['run:server']);
+
+  grunt.registerTask('deploy', 'aws_s3:dist');
 };
