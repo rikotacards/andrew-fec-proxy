c915bda0ba06d3e74e49b5dcc2812e1b68e6c02d
ahchung23 <hahneol@gmail.com>
2019-04-11 05:03:19 -0400

    try configuring grunt with jshint

--- modules/andrew-service-details/Gruntfile.js
+++ modules/andrew-service-details/Gruntfile.js
@@ -1,18 +1,56 @@
 // eslint-disable-next-line func-names
 module.exports = function (grunt) {
-  // grunt.initConfig({
-  //   pkg: grunt.file.readJSON('package.json'),
-  //   concat: {
 
-  //   }
-  // })
-  grunt.registerTask('speak', function() {
-    console.log("I'm speaking");
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['client-details/src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    'jshint-jsx': {
+      files: ['Gruntfile.js', 'client-details/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        },
+        esnext: true,
+        convertJSX: true,
+        reporterOutput: "",
+      }
+    },
+    watch: {
+      files: ['<%= jshint-jsx.files %>'],
+      tasks: ['jshint-jsx']
+    }
   });
-  grunt.registerTask('yell', function () {
-    console.log("I'm YELLING!");
-  });
 
-  // grunt.registerTask('s')
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint-jsx');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-}
+  grunt.registerTask('test', ['jshint-jsx']);
+
+  grunt.registerTask('default', ['jshint-jsx', 'concat', 'uglify']);
+
+};
